3f83ed7d90f3a0dbb78d37533c3acf2ef29afd35
gbachik <gbachik@gmail.com>
2014-07-22 17:45:15 -0600

    /Users/student/githubPush

---
+++ main.js
@@ -0,0 +1,15 @@
+var chunk = function(array, chunks){
+	var isEven = ((chunks%2)===0);
+	if (isEven) {
+		var output = [];
+		var howToSplit = array.length / chunks;
+		for (var i = 0; i < chunks; i++) {
+			output.push(array.splice(0,howToSplit));
+		};
+
+		console.log(output);
+	}
+};
+
+var input = [1,2,3,4,5,6,7,8,9,10];
+chunk(input, 2) // should return [ [1,2,3,4,5], [6,7,8,9,10] ]
